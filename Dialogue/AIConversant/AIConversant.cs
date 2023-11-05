8171995bc3ca8a5e74eaa7f4b66d21477b9a95a8
Hubert Zambrzycki <107870354+hZambrzycki@users.noreply.github.com>
2023-11-05 19:02:59 +0100

    Add files via upload

---
+++ Dialogue/AIConversant/AIConversant.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Control;
+using RPG.Stats;
+using UnityEngine;
+
+namespace RPG.Dialogue
+{
+    public class AIConversant : MonoBehaviour, IRayCastable
+    {
+        [SerializeField] Dialogue dialogue = null;
+        [SerializeField] string conversantName;
+
+        public CursorType GetCursorType()
+        {
+            return CursorType.Dialogue;
+        }
+
+        public bool HandleRaycast(PlayerController callingController)
+        {
+            if (dialogue == null)
+            {
+                return false;
+            }
+
+            Health health = GetComponent<Health>();
+            if (health && health.IsDead()) return false;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                callingController.GetComponent<PlayerConversant>().StartDialogue(this, dialogue);
+            }
+            return true;
+        }
+
+        public string GetName()
+        {
+            return conversantName;
+        }
+
+    }
+
+}
+
